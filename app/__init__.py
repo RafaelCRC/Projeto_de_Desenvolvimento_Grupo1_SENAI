fe61978f6a1fe4e176adbded31e298c19dc3223a
Gleice Lisbino <gleice.lisbino@liferay.com>
2021-11-30 13:50:31 -0300

    Add Colaborador

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from app.main.projeto.projeto_controller import api as projeto_ns
 from app.main.squad.squad_controller import api as squad_ns
 from app.main.custos.custos_controller import api as custos_ns
 from app.main.cargos.cargos_controller import api as cargos_ns
+from app.main.colaborador.colaborador_controller import api as colaborador_ns
 from app.main.pacotedeservicos.pacotedeservicos_controller import api as pacote_ns
 from app.main.vagas.vagas_controller import api as vagas_ns
 from app.main.contadeacesso.contadeacesso_controller import api as conta_ns
@@ -32,4 +33,5 @@ api.add_namespace(pacote_ns, path='/pacote')
 api.add_namespace(vagas_ns, path='/vagas')
 api.add_namespace(custos_ns, path='/costs')
 api.add_namespace(cargos_ns, path='/cargos')
+api.add_namespace(colaborador_ns, path='/colaborador')
 api.add_namespace(conta_ns, path='/conta')
---
+++ app/main/colaborador/colaborador_controller.py
@@ -0,0 +1,55 @@
+from flask_restplus import Resource, Namespace, fields
+from flask import request
+from app.main.colaborador.colaborador_db import ColaboradorDb
+
+api = Namespace('Colaborador', description="Gerenciamento de Colaboradores")
+modelo = api.model('CargosModel', {
+    'id': fields.String,
+    'cpf': fields.String,
+    'nome': fields.String,
+    'matricula': fields.String,
+    'telefone': fields.String,
+    'emailPessoal': fields.String,
+    'emailInstitucional': fields.String,
+    'lattes': fields.String
+})
+
+
+@api.route('/')
+class PessoaController(Resource):
+    @api.response(200, "Found with success")
+    def get(self):
+        return ColaboradorDb.obter(), 200
+
+    @api.expect(modelo)
+    def post(self):
+        return ColaboradorDb.adicionar(request.json), 201
+
+
+@api.route('/query/<query>')
+class PessoaController(Resource):
+    @api.response(200, "Found with success")
+    def get(self, query:str):
+        return ColaboradorDb.querySearch(str(query)), 200
+
+
+@api.route('/<id>')
+class PessoaIdController(Resource):
+    @api.response(200, "Found with success")
+    def get(self, id:int):
+        return ColaboradorDb.obter(str(id)), 200
+
+
+@api.route('/<id>')
+class PessoaIdController(Resource):
+    @api.param('id', 'Códgio identificador')
+    @api.param('nome', 'Nome do colaborador')
+    @api.param('cpf', "CPF do colaborador")
+    def put(self, id: str):
+        return ColaboradorDb.adicionar(str(id), request.json), 201
+
+@api.route('/<id>')
+class PessoaIdController(Resource):
+    def delete(self, id: str):
+        return ColaboradorDb.remover(int(id)), 200
+
