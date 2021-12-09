0ccf6263ab8f3fe52e584393a70205cc990d9428
João Tavares <coelhotavares1711@gmail.com>
2021-12-08 21:07:37 -0300

    Fixes em Cargo

--- app/main/cargos/cargos_controller.py
+++ app/main/cargos/cargos_controller.py
@@ -12,33 +12,31 @@ modelo = api.model('CargosModel', {
     'funcao': fields.String
 })
 
-
 @api.route('/')
 class CargosController(Resource):
     @api.response(200, "Found with success")
+    @api.param("qtdItens", "Quantidade de itens por página")
+    @api.param("pagina", "Número da página")
     def get(self):
-        descricao = None
         qtdItens = None
         pagina = None
         if request.method == 'GET':
-            if 'descricao' in request.args:
-                descricao = request.args['descricao']
-            if qtdItens in request.args:
+            if 'qtdItens' in request.args:
                 qtdItens = request.args['qtdItens']
-            if pagina in request.args:
+            if 'pagina' in request.args:
                 pagina = request.args['pagina']
-        return CargosDb.obter(None, descricao, qtdItens, pagina), 200
+        return CargosDb.obter(None, qtdItens, pagina, titulo=True), 200
 
     @api.expect(modelo)
     def post(self):
         return CargosDb.adicionar(request.json), 201
 
 
-@api.route('/<nome>')
-class CargosIdController(Resource):
+@api.route('/findByName/<nome>')
+class CargosNomeController(Resource):
     @api.response(200, "Busca realizada com sucesso")
     def get(self, nome: str):
-        return CargosDb.obter(nome=nome), 200
+        return CargosDb.obter(nome=nome, titulo=False), 200
 
 
 @api.route('/squad/<id>')
@@ -49,20 +47,17 @@ class CargosAndSquadIdController(Resource):
 
 
 @api.route('/<id>')
-class CargosIdController(Resource):
-    @api.param('id', 'Códgio identificador')
-    @api.param('nome', 'Título do cargo')
-    @api.param('funcao', "Trabalho a ser executado pelo cargo")
-    def put(self, id: str):
-        return CargosDb.adicionar(str(id), request.json), 200
-
-@api.route('/<id>')
 class CargosRemovalIdController(Resource):
-    def delete(self, id: str):
-        return CargosDb.remover(int(id)), 200
+    def delete(self, id):
+        return CargosDb.remover(id), 200
 
-    def get(self, id: int):
-        return CargosDb.obter(str(id=id)), 200
+    def get(self, id: str):
+        return CargosDb.obter(id=id, titulo=False), 200
+
+    @api.response(200, "Busca realizada com sucesso")
+    @api.expect(modelo)
+    def put(self, id):
+        return CargosDb.alterar(id, request.json), 201
 
 
 """
@@ -72,3 +67,4 @@ class PessoaIdController(Resource):
     def get(self, id:int):
         return CargosDb.cargoSkill(int(id)), 200
 """
+
